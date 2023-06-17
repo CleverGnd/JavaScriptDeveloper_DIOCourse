32af41be8da1e96cddee7dbc59fb23e11a4a5e81
Cleverson Guandalin <cleversongnd@gmail.com>
2023-06-16 21:09:41 -0300

    Primeiros Passos Com JavaScript

---
+++ 01_Aprenda a Programar/01_Primeiros Passos Com JavaScript/aula01.js
@@ -0,0 +1,10 @@
+// Aula 01 - Primeiros Passos Com JavaScript
+
+// Criando uma variável, let é uma variável que pode ser alterada
+let variavel = 10 / 10;
+
+// Const é uma variável que não pode ser alterada
+const pi = 3.14;
+
+// Mostrando o resultado no console, console.log é uma função
+console.log(variavel);
---
+++ 01_Aprenda a Programar/01_Primeiros Passos Com JavaScript/desafio01.js
@@ -0,0 +1,19 @@
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+1 - Preço do combustível;
+2 - Gasto médio de combustível do carro por KM;
+3 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem. */
+
+// Resposta:
+
+let preco_combustivel = 4.50;
+let distancia_viagem = 100;
+
+const gasto_medio_combustivel = 10;
+
+valor_gasto = (distancia_viagem / gasto_medio_combustivel) * preco_combustivel;
+
+console.log(valor_gasto);
